ce132b06fef6815043b263b0cbab2b9e76ca21be
Иван <kleimenov416@gmail.com>
2021-02-23 13:36:19 -0500

    another attempt

--- server/Server.js
+++ server/Server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000; //$ export PORT=3030
 const app = express();
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../src/build')));
+
 
 const dataPath = path.join(__dirname, "data.json");
 
@@ -20,9 +20,7 @@ app.get("/data", (req, res) => {
   });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/build/index.html'));
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening ${PORT}............./`);
