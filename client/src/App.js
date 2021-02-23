a623ef237425eab53d18f98f38877aa9bac22f30
Иван <kleimenov416@gmail.com>
2021-02-23 14:13:11 -0500

    change project folders structure

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   }, []);
 
   const getData = async () => {
-    const response = await fetch("https://hhcd-app.herokuapp.com/data");
+    const response = await fetch("http://localhost:3030/data");
     const data = await response.json();
     setData(data.gaugeData);
   };
