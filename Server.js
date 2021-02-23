2a3b5a997d1769028015998467362a9016f04d7b
Иван <kleimenov416@gmail.com>
2021-02-23 16:08:59 -0500

    a

--- Server.js
+++ Server.js
@@ -21,6 +21,16 @@ app.get("/data", (req, res) => {
 });
 
 
+// --> Add this
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening ${PORT}............./`);
